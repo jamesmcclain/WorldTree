e98e64ffb33e0f79b5139cb0d1a13baf8e92e61b
James W. McClain <jmcclain@daystrom-data-concepts.com>
2014-11-16 00:56:50 +0000

    Queries and random queries.

--- src/worldtree/index.clj
+++ src/worldtree/index.clj
@@ -1,9 +1,10 @@
 (ns ^{:author "James McClain <jwm@daystrom-data-concepts.com>"}
   worldtree.index
-  (:require [worldtree.series :as series]))
+  (:require  [clojure.string :as string]
+             [worldtree.series :as series]))
 
 ;; Build a search index for the given dataset in the given directory.
-(defn build-index [dataset dir]
+(defn build [dataset dir]
   (letfn [(is-dir? [thing] (.isDirectory (java.io.File. thing)))
           (exists? [thing] (.exists (java.io.File. thing)))]
     (let [{m :m n :n chunks :chunks} dataset
@@ -25,3 +26,35 @@
                      sorted-lists
                      (concat (next sorted-lists) (list (series/compute-rankings dataset (dec (dec m))))))))
       (println (java.util.Date.)))))
+
+;; Query a search index.
+(defn query [dir k t]
+  (let [frame (read-string (slurp (str dir "/" k "/" (int t))))]
+    (loop [topk (set (:chunk-list frame)) changes (:change-list frame)]
+      (let [change (first changes)]
+        (if (or (nil? change) (>= (:T+t change) t))
+          ;; no more changes and/or past query time, return topk
+          (do
+            (assert (== (count topk) k))
+            topk)
+          ;; update the topk and iterate again
+          (let [[out in] (:ij change)]
+            (recur (conj (disj topk out) in) (rest changes))))))))
+
+;; Do n random queries.  This function may only work on Linux (it
+;; certainly requires a /proc filesystem).
+(defn random-queries [dir k t n]
+  (letfn [(get-io [] ; read information out of /proc/$pid/io
+            (let [pid (first (string/split (.getName (java.lang.management.ManagementFactory/getRuntimeMXBean)) #"@"))
+                  [rchar _ syscr _ _ _ _] (map #(Double/parseDouble %) (re-seq #"\d+" (slurp (str "/proc/" pid "/io"))))]
+              [rchar syscr]))
+          (get-millis [] ; current time in milliseconds
+            (double (.getTimeInMillis (java.util.Calendar/getInstance))))
+          (get-vitals [] ; concatenate proc stuff and millis together
+            (conj (get-io) (get-millis)))]
+    (let [before (get-vitals)]
+      (dotimes [_ n]
+        (query dir k (rand (dec t))))
+      (let [after (get-vitals)]
+        ; rchars per query, syscr per query, milliseconds per query
+        (map #(/ % n) (map - after before))))))
